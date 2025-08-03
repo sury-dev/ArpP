032dbd28566acd427770d45583bd59f90852a5bb
Suryansh Pandey <coderhood0011@gmail.com>
2025-08-03 21:39:36 +0530

    Front

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const baseURL =
-  import.meta.env.VITE_API_BASE_URL ;
+const baseURL = '/api';
 const instance = axios.create({
   baseURL,
   withCredentials: true,
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
 })
